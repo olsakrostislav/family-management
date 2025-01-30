d472ccfcbcafa9d148d2a7f0558a59efbac1d065
Rostislav <olsakrostislav@gmail.com>
2025-01-30 18:49:23 +0100

    search fix

--- server/src/controllers/searchController.ts
+++ server/src/controllers/searchController.ts
@@ -5,12 +5,18 @@ const prisma = new PrismaClient();
 
 export const search = async (req: Request, res: Response): Promise<void> => {
   const { query } = req.query;
+
+  if (!query || typeof query !== "string") {
+    res.status(400).json({ message: "Query parameter is required" });
+    return;
+  }
+
   try {
     const tasks = await prisma.task.findMany({
       where: {
         OR: [
-          { title: { contains: query as string } },
-          { description: { contains: query as string } },
+          { title: { contains: query, mode: "insensitive" } },
+          { description: { contains: query, mode: "insensitive" } },
         ],
       },
     });
@@ -18,17 +24,18 @@ export const search = async (req: Request, res: Response): Promise<void> => {
     const projects = await prisma.project.findMany({
       where: {
         OR: [
-          { name: { contains: query as string } },
-          { description: { contains: query as string } },
+          { name: { contains: query, mode: "insensitive" } },
+          { description: { contains: query, mode: "insensitive" } },
         ],
       },
     });
 
     const users = await prisma.user.findMany({
       where: {
-        OR: [{ username: { contains: query as string } }],
+        OR: [{ username: { contains: query, mode: "insensitive" } }],
       },
     });
+
     res.json({ tasks, projects, users });
   } catch (error: any) {
     res.status(500).json({ message: `Error searching: ${error.message}` });
